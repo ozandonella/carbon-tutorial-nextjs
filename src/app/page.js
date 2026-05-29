c8bcca02c03729260a9bae48b1b377aa751d3d5a
OscarZandonella <Oscar.Zandonella@ibm.com>
2026-05-29 17:28:38 -0400

    feat(tutorial): complete step 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import LandingPage from './home/page';
+import { Grid, Column } from '@carbon/react';
 
 export default function Page() {
-    return <LandingPage />;
-  }
+  return <LandingPage />;
+}
